959f479fa464224be245599f86d8fa4e2077b4c0
caiocoswosk <caiocoswosk@gmail.com>
2023-10-19 20:07:57 -0300

    :sparkles: feat: product CRUD

---
+++ src/main/java/com/example/productsapi/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.productsapi.repositories;
+
+import com.example.productsapi.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
